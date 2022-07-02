d87f62ff606e7feee23609a8b159860c55693423
danyllorobemar <danyllo.robemar@gmail.com>
2022-07-02 17:26:11 -0300

    adding the project

---
+++ src/model/services/ContractService.java
@@ -0,0 +1,34 @@
+package model.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import model.entities.Contract;
+import model.entities.Installment;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+	
+	public ContractService(OnlinePaymentService onlinePaymentService ) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, Integer months) {
+		double basicQuota = contract.getTotalValue() / months;
+		for (int i=1; i<=months; i++) {
+			Date date = addMonths(contract.getDate(), i);
+            double updatedQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+            double fullQuota =  updatedQuota + onlinePaymentService.paymentFee(updatedQuota);
+            contract.addInstallment(new Installment(date, fullQuota));
+		}
+	}
+	
+	private Date addMonths(Date date, int n) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(calendar.MONDAY, n);
+		return calendar.getTime();
+	}
+
+}
